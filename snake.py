980d3954063773c4d1004b2201071329d3a5409e
Isuru Wimalasiri <isuru.wimalasiri1997@gmail.com>
2023-05-14 03:12:12 +0530

    add higher score feature to the scoreboard

--- snake.py
+++ snake.py
@@ -18,7 +18,6 @@ class Snake:
         for position in STARTING_POSITIONS:
             self.add_segment(position)
 
-
     def add_segment(self, position):
         new_segment = Turtle("square")
         new_segment.color("white")
@@ -26,6 +25,12 @@ class Snake:
         new_segment.goto(position)
         self.segments.append(new_segment)
 
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(1000, 1000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
 
     def extend(self):
         # Add a new segment to the snake
